4a71e36810d3f597c5da688af039d50668e17681
zaryab2000 <zaryabafser2000@gmail.com>
2020-08-31 09:18:21 +0530

    started FactoryCOntract

--- contracts/FactoryContract.sol
+++ contracts/FactoryContract.sol
@@ -1,6 +1,29 @@
-pragma solidity >= 0.5.0 < 0.7.0;
+pragma solidity ^0.5.1;
+
+import "./SafeMath.sol";
+import "./MovieContract.sol";
 
 contract FactoryContract {
-  constructor() public {
-  }
+  
+    using SafeMath for uint256;
+
+	uint256 public contractCount;
+	address[] public contractContainer;
+	address public recentContract;
+
+	mapping(uint256 => address) public IdToMovieContracts;
+
+	function newMovieContract(string memory _movieName) public returns(MovieContract){
+		MovieContract instance = new MovieContract(_movieName,msg.sender);
+		recentContract = address(instance);
+		contractCount++;
+		contractContainer.push(address(instance));
+		IdToMovieContracts[contractCount] = address(instance);
+
+	}
+
+	function  checkSpecificContract(uint256 _id) public view returns(address){
+		return IdToMovieContracts[_id];
+	}
+
 }
