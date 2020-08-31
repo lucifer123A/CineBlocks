ad3d27ca148d24b6ce1b460c4b7ca6cc3642ee47
zaryab2000 <zaryabafser2000@gmail.com>
2020-08-31 08:43:57 +0530

    started MovieContract

--- contracts/MovieContract.sol
+++ contracts/MovieContract.sol
@@ -1,6 +1,49 @@
-pragma solidity >= 0.5.0 < 0.7.0;
+pragma solidity ^0.5.1;
+
+import "./SafeMath.sol";
 
 contract MovieContract {
-  constructor() public {
+  using SafeMath for uint256;
+
+  address payable public owner;
+  address public tokenAddress;
+  string public movieName;
+  string public tokenName;
+  uint256 public deadline;
+  uint256 public totalSupply;
+  uint256 public totalTokenSold;
+  uint256 public tokenPrice = 1000;
+  uint256 public creationDate;
+  uint256 public investorCount;
+  uint256 public withdrawCount;
+
+    
+
+
+constructor(string memory _movieName, address payable _movieCreator) public{
+    owner = _movieCreator;
+    movieName = _movieName;
+    creationDate = now;
   }
+
+  struct movieDetails{
+    string name;
+    string details;
+    string ipfsHash;
+  }
+
+  struct investorDetails{
+    string name;
+    string contactDetails;
+    address payable investorAddress;
+    uint256 tokensBought;
+  }
+
+  struct withdrawDetails{
+    address withdrawnBy;
+    uint256 amountWithdrawn;
+    uint256 time;
+    string reason;
+  }
+
 }
